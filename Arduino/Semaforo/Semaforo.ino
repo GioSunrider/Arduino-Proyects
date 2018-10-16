b1a6a2ec05e80d1e413779040ef5fd55bf90f760
Jorge Ramos <10605193+GioSunrider@users.noreply.github.com>
2018-10-16 10:52:16 +0200

    Add files via upload

---
+++ Arduino/Semaforo/Semaforo.ino
@@ -0,0 +1,38 @@
+int rojo=11;      
+int amarillo=12;  
+int verde=13;     
+int azul=5;
+int naranja=7;
+int interruptor=3;
+int valor=0;
+
+void setup() {
+  pinMode(verde,OUTPUT);   
+  pinMode(amarillo,OUTPUT);
+  pinMode(rojo,OUTPUT);    
+  pinMode(azul,OUTPUT);
+  pinMode(naranja,OUTPUT);
+  pinMode(interruptor, INPUT); 
+}
+
+void loop() {
+ digitalWrite(verde,HIGH); 
+ digitalWrite(naranja,HIGH);
+ valor=digitalRead(interruptor);
+  if ( valor==HIGH){
+   delay(500);
+   digitalWrite(verde,LOW); 
+   digitalWrite(amarillo,HIGH);
+   delay(1000);
+   digitalWrite(naranja,LOW);
+   delay(500);
+   digitalWrite(amarillo,LOW); 
+   digitalWrite(rojo,HIGH); 
+   delay(200); 
+   digitalWrite(azul,HIGH); 
+   delay(5000);              
+   digitalWrite(rojo,LOW);  
+   digitalWrite(azul,LOW);  
+   delay(500); 
+  } 
+}
